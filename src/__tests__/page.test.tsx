20ec1f7345c9fb630bec03593177e2e0ff6d6226
AleksandrKlesh <alex73kl@gmail.com>
2025-09-06 18:10:28 +0700

    fix: modify tests for using next-intl

--- src/__tests__/page.test.tsx
+++ src/__tests__/page.test.tsx
@@ -1,12 +1,20 @@
 import { expect, test, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import Page from '@/app/page';
+import Page from '@/app/[locale]/page';
+import { NextIntlClientProvider } from 'next-intl';
 
 vi.mock('@/firebase/firebase', () => ({
   registerWithEmailAndPassword: vi.fn(),
 }));
 
 test('Page', () => {
-  render(<Page />);
+  render(
+    <NextIntlClientProvider
+      locale="en"
+      messages={{ Test: { home: 'Home', about: 'About' } }}
+    >
+      <Page />
+    </NextIntlClientProvider>
+  );
   expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined();
 });
