b6f769e00f2416458c0b7031b296275fefdd849c
AleksandrKlesh <alex73kl@gmail.com>
2025-09-06 23:38:08 +0700

    feat: add tests for error and not-found components

---
+++ src/__tests__/global-not-found.test.tsx
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom';
+
+import NotFound from '@/app/global-not-found';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+describe('Global not found', () => {
+  it('renders not found heading', () => {
+    render(<NotFound />);
+
+    expect(
+      screen.getByRole('heading', { name: /404 - page not found/i })
+    ).toBeInTheDocument();
+  });
+
+  it('renders message', () => {
+    render(<NotFound />);
+
+    expect(
+      screen.getByText(/oops! the page you’re looking for doesn’t exist/i)
+    ).toBeInTheDocument();
+  });
+});
