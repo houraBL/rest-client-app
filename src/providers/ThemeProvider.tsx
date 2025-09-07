39a33d76712e88d04c4181d1db56dbf2b8026c95
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-09-07 22:47:55 +0800

    feat: add theme provider

---
+++ src/providers/ThemeProvider.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import useLocalStorage from '@/hooks/useLocalStorage';
+import { type ReactNode, useEffect, useState } from 'react';
+
+import { type Theme, ThemeContext } from './ThemeContext';
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [localTheme, setLocalTheme] = useLocalStorage('theme');
+  const [theme, setTheme] = useState<Theme>(
+    localTheme === 'dark' ? 'dark' : 'light'
+  );
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (theme === 'dark') {
+      root.setAttribute('data-theme', 'dark');
+      root.classList.add('dark');
+    } else {
+      root.setAttribute('data-theme', 'light');
+      root.classList.remove('dark');
+    }
+    setLocalTheme(theme);
+  }, [setLocalTheme, theme]);
+
+  const toggleTheme = () =>
+    setTheme((prev) => (prev === 'dark' ? 'light' : 'dark'));
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+export { ThemeContext };
