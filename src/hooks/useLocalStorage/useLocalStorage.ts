e3463f6452f8b4c12202fc3e9ffcf0f91087ddfc
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-09-07 22:45:31 +0800

    feat: add useLocalStorage and useTheme hooks

---
+++ src/hooks/useLocalStorage/useLocalStorage.ts
@@ -0,0 +1,38 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export const LS_KEYS = {
+  THEME: 'theme',
+  SEARCH: 'search',
+};
+
+const useLocalStorage = (
+  key: string,
+  initialValue: string = ''
+): [string, React.Dispatch<React.SetStateAction<string>>] => {
+  const [value, setValue] = useState<string>(initialValue);
+
+  useEffect(() => {
+    try {
+      const stored = window.localStorage.getItem(key);
+      if (stored !== null) {
+        setValue(stored);
+      }
+    } catch (err) {
+      console.error('useLocalStorage get error:', err);
+    }
+  }, [initialValue, key]);
+
+  useEffect(() => {
+    try {
+      window.localStorage.setItem(key, value);
+    } catch (err) {
+      console.error('useLocalStorage set error:', err);
+    }
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
