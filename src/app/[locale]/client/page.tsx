8144b5976d361a8215b5a5c75ab604874224108e
AleksandrKlesh <alex73kl@gmail.com>
2025-09-09 22:47:53 +0700

    feat: add clientPage to routing

--- src/app/[locale]/client/page.tsx
+++ src/app/[locale]/client/page.tsx
@@ -1,7 +1,10 @@
-export default function ClientPage() {
+import { ClientPage } from '@/components/Client/ClientPage';
+
+export default function Client() {
   return (
     <div className="text-center py-20">
       <h2 className="text-2xl font-bold">Client</h2>
+      <ClientPage />
     </div>
   );
 }
