b9aebd603037f99bb5a3f3ad567542365494b991
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-09-13 06:18:19 +0800

    feat: implement function that makes api call, logs data to firebase db

---
+++ src/utils/makeApiCall.ts
@@ -0,0 +1,74 @@
+import { logUserRequest } from '@/firebase/logUserRequest';
+
+export async function makeApiCall({
+  url,
+  method,
+  requestBody,
+  headers = {},
+}: {
+  url: string;
+  method: string;
+  requestBody?: string;
+  headers?: Record<string, string>;
+}): Promise<{
+  status: number;
+  data?: string;
+  error?: string;
+}> {
+  const startTime = Date.now();
+  let response: Response | undefined;
+  let status = 0;
+  let responseSize = 0;
+  let errorDetails: string | null = null;
+  let requestSize = 0;
+  let duration = 0;
+  let responseText = '';
+
+  try {
+    response = await fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers,
+      },
+      body: requestBody ? requestBody : undefined,
+    });
+
+    status = response.status;
+    responseText = await response.text();
+    responseSize = new TextEncoder().encode(responseText).length;
+
+    if (!response.ok) {
+      errorDetails = `HTTP Error: ${status} - ${responseText.substring(0, 100)}`;
+    }
+  } catch (error) {
+    console.log(error);
+    if (error instanceof Error) {
+      errorDetails = `Network/Fetch Error: ${error.message}`;
+    } else {
+      errorDetails = 'Unknown error';
+    }
+  } finally {
+    const endTime = Date.now();
+    duration = endTime - startTime;
+
+    requestSize = requestBody
+      ? new TextEncoder().encode(JSON.stringify(requestBody)).length
+      : 0;
+    await logUserRequest({
+      requestDuration: duration,
+      responseStatusCode: status,
+      requestMethod: method,
+      requestSize: requestSize,
+      responseSize: responseSize,
+      endpointUrl: url,
+      errorDetails: errorDetails,
+    });
+  }
+
+  return {
+    status,
+    data: JSON.parse(responseText),
+    error: errorDetails ?? undefined,
+  };
+}
