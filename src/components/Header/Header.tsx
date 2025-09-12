3993dc021c775907cf74e6c2069fee53e4711fe6
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-09-13 06:13:35 +0800

    feat: add mock values and buttons to check functionality of firebase api functions

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,43 +1,148 @@
 'use client';
+import { auth } from '@/firebase/firebase';
+import { logUserRequest, RequestLogEntry } from '@/firebase/logRequest';
 import useTheme from '@/hooks/useTheme';
 import { Link } from '@/i18n/navigation';
+import { makeApiCall } from '@/utils/makeApiCall';
+import { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+const mockRequestLogData: RequestLogEntry[] = [
+  {
+    requestDuration: 80,
+    responseStatusCode: 200,
+    requestMethod: 'GET',
+    requestSize: 128,
+    responseSize: 1024,
+    endpointUrl: 'https://api-1-one.com/users/1',
+  },
+
+  {
+    requestDuration: 123,
+    responseStatusCode: 500,
+    requestMethod: 'POST',
+    requestSize: 512,
+    responseSize: 0,
+    endpointUrl: 'https://api-2-two.com/users',
+    errorDetails: 'TimeoutError',
+  },
+
+  {
+    requestDuration: 123,
+    responseStatusCode: 200,
+    requestMethod: 'GET',
+    requestSize: 456,
+    responseSize: 789,
+    endpointUrl: 'https://api-3-three.com/users/1',
+  },
+];
+
+type ApiRequest = {
+  url: string;
+  method: string;
+  requestBody?: string;
+  headers?: Record<string, string>;
+};
+
+const mockRequests: ApiRequest[] = [
+  {
+    url: 'https://rickandmortyapi.com/api/character/1',
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  },
+  {
+    url: 'https://rickandmortyapi.com/api/character',
+    method: 'POST',
+    requestBody: JSON.stringify({
+      name: 'AI Morty',
+      status: 'Alive',
+      species: 'Robot',
+    }),
+    headers: { Accept: 'application/json' },
+  },
+
+  {
+    url: 'https://rickandmortyapi.com/api/character?name=rick&status=alive',
+    method: 'GET',
+    headers: { Accept: 'application/json' },
+  },
+];
 
 export default function Header() {
-  const { toggleTheme } = useTheme();
+  const [user, loading, error] = useAuthState(auth);
+  const [res, setRes] = useState<
+    | {
+        status: number;
+        data?: string;
+        error?: string;
+      }
+    | undefined
+  >(undefined);
   return (
-    <header className="navbar bg-base-100 z-40 sticky top-0  mb-32 shadow-lg">
-      <div className="flex-1">
-        <Link href="/" className="btn btn-ghost text-xl">
-          RESTful client
-        </Link>
+    <>
+      <header className="navbar bg-base-100 z-40 sticky top-0  mb-32 shadow-lg">
+        <div className="flex-1">
+          <Link href="/" className="btn btn-ghost text-xl">
+            RESTful client
+          </Link>
+        </div>
+        <button
+          className="btn"
+          onClick={() => logUserRequest(mockRequestLogData[0])}
+        >
+          send 0
+        </button>
+        <button
+          className="btn"
+          onClick={() => logUserRequest(mockRequestLogData[1])}
+        >
+          send 1
+        </button>
+        <button
+          className="btn"
+          onClick={() => logUserRequest(mockRequestLogData[2])}
+        >
+          send 2
+        </button>
+        <button
+          className="btn"
+          onClick={async () => {
+            const result = await makeApiCall(mockRequests[0]);
+            setRes(result);
+          }}
+        >
+          request 0
+        </button>
+        <button
+          className="btn"
+          onClick={async () => {
+            const result = await makeApiCall(mockRequests[1]);
+            setRes(result);
+          }}
+        >
+          request 1
+        </button>
+        <button
+          className="btn"
+          onClick={async () => {
+            const result = await makeApiCall(mockRequests[2]);
+            setRes(result);
+          }}
+        >
+          request 2
+        </button>
+      </header>
+      <div>
+        {res && (
+          <div>
+            <label>{res.status}</label>
+            <p>{JSON.stringify(res.data)}</p>
+            <p>{res.error}</p>
+          </div>
+        )}
       </div>
-      <label className="swap swap-rotate">
-        {/* this hidden checkbox controls the state */}
-        <input
-          type="checkbox"
-          className="theme-controller"
-          value="synthwave"
-          onClick={toggleTheme}
-        />
-
-        {/* sun icon */}
-        <svg
-          className="swap-off h-10 w-10 fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-        </svg>
-
-        {/* moon icon */}
-        <svg
-          className="swap-on h-10 w-10 fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-        </svg>
-      </label>
-    </header>
+    </>
   );
 }
