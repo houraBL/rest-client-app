ae547fdd9888fa8cf848e90c86ba028edcfce207
AleksandrKlesh <alex73kl@gmail.com>
2025-09-13 19:09:22 +0700

    feat: add store to providers

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import { ThemeProvider } from '@/providers/ThemeProvider';
+import { Provider } from 'react-redux';
+import { store } from '@/store/store';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <Provider store={store}>
+      <ThemeProvider>{children}</ThemeProvider>;
+    </Provider>
+  );
 }
