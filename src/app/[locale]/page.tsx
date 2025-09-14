e3f05caa5b8fe463c7ecb906160259e749ec4af7
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-09-14 18:15:02 +0800

    fix: fix basic page layout

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,9 @@
-import { useTranslations } from 'next-intl';
-import Link from 'next/link';
+import Main from '@/components/Main/Main';
 
 export default function Home() {
-  const t = useTranslations();
   return (
     <div className="font-sans grid items-center justify-items-center p-8 pb-20 gap-4 sm:p-20">
-      <h1>{t('Test.home')}</h1>
-      <Link href="/history">{t('Test.history')}</Link>
-      <Link href="/about">{t('Test.about')}</Link>
+      <Main />
     </div>
   );
 }
