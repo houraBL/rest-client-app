016080fbc56552c50a37e430fb7db8b0b60f5932
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-09-16 08:52:09 +0800

    test: update NotFound tests with translation

--- src/__tests__/not-found.test.tsx
+++ src/__tests__/not-found.test.tsx
@@ -2,22 +2,24 @@ import '@testing-library/jest-dom';
 
 import NotFound from '@/app/not-found';
 import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
+
+vi.mock('next-intl', async (importActual) => {
+  const actual = await importActual<typeof import('next-intl')>();
+  return {
+    ...actual,
+    useTranslations: () => (key: string) => key,
+  };
+});
 
 describe('Global not found', () => {
-  it('renders not found heading', () => {
+  it('renders not found page correctly', () => {
     render(<NotFound />);
 
-    expect(
-      screen.getByRole('heading', { name: /404 - page not found/i })
-    ).toBeInTheDocument();
-  });
+    expect(screen.getByRole('heading', { name: /title/i })).toBeInTheDocument();
 
-  it('renders message', () => {
-    render(<NotFound />);
+    expect(screen.getByText(/description/i)).toBeInTheDocument();
 
-    expect(
-      screen.getByText(/oops! the page you’re looking for doesn’t exist/i)
-    ).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /backHome/i })).toBeInTheDocument();
   });
 });
