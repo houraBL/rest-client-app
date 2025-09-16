b7e810291e0ee51d4642fa88853a58fd14331c77
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-09-16 08:34:45 +0800

    feat: add AuthProvider to Providers

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -3,11 +3,14 @@
 import { ThemeProvider } from '@/providers/ThemeProvider';
 import { Provider } from 'react-redux';
 import { store } from '@/store/store';
+import { AuthProvider } from '@/providers/AuthProvider';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
-      <ThemeProvider>{children}</ThemeProvider>;
+      <AuthProvider>
+        <ThemeProvider>{children}</ThemeProvider>
+      </AuthProvider>
     </Provider>
   );
 }
