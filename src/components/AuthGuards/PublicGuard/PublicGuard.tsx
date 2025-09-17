6321cf06e5e2fe87236d0ec5ea4ae7d91a479797
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-09-17 18:16:00 +0800

    feat: add PrivateGuard and PublicGuard with tests to check for user auth status

---
+++ src/components/AuthGuards/PublicGuard/PublicGuard.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { Loader } from '@/components/Loader/Loader';
+import { useAuth } from '@/hooks/useAuth/useAuth';
+import { useRouter } from '@/i18n/navigation';
+import { ReactNode, useEffect } from 'react';
+
+export default function PublicGuard({ children }: { children: ReactNode }) {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+  useEffect(() => {
+    if (user) router.push('/');
+  }, [router, user]);
+
+  if (loading || user) {
+    return (
+      <div className="py-20 text-center">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (user) router.push('/');
+
+  return <>{children}</>;
+}
