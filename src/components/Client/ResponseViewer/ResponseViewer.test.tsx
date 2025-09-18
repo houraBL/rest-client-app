54529cb596903c2c4a1ab86e0807a2df08a35062
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-09-19 04:54:09 +0800

    test: update ResponseViewer to have theme provider in tests

--- src/components/Client/ResponseViewer/ResponseViewer.test.tsx
+++ src/components/Client/ResponseViewer/ResponseViewer.test.tsx
@@ -2,35 +2,47 @@ import { describe, expect, it, vi } from 'vitest';
 import ResponseViewer from './ResponseViewer';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { ReactNode } from 'react';
+import { ThemeProvider } from '@/providers/ThemeProvider';
 
 vi.mock('@/components/Loader/Loader', () => ({
   Loader: () => <div>Loading</div>,
 }));
 
+const renderWithTheme = (children: ReactNode) => {
+  render(<ThemeProvider>{children}</ThemeProvider>);
+};
+
 describe('ResponseViewer', () => {
   it('renders loader when loading', () => {
-    render(<ResponseViewer response={{ status: 200, loading: true }} />);
+    renderWithTheme(
+      <ResponseViewer response={{ status: 200, loading: true }} />
+    );
     expect(screen.getByText('Loading')).toBeDefined();
   });
 
   it('renders HTTP success status correctly', () => {
-    render(<ResponseViewer response={{ status: 201, data: '{"ok":true}' }} />);
+    renderWithTheme(
+      <ResponseViewer response={{ status: 201, data: '{"ok":true}' }} />
+    );
     expect(screen.getByText('Response Status: 201')).toBeDefined();
     const statusDiv = screen.getByText(/Response Status:/);
-    expect(statusDiv).toHaveClass('bg-green-100');
-    expect(statusDiv).toHaveClass('text-green-700');
+    expect(statusDiv).toHaveClass('badge-success');
   });
 
   it('renders HTTP error status correctly', () => {
-    render(<ResponseViewer response={{ status: 400, data: '{"ok":true}' }} />);
+    renderWithTheme(
+      <ResponseViewer response={{ status: 400, data: '{"ok":true}' }} />
+    );
     expect(screen.getByText('Response Status: 400')).toBeDefined();
     const statusDiv = screen.getByText(/Response Status:/);
-    expect(statusDiv).toHaveClass('bg-red-100');
-    expect(statusDiv).toHaveClass('text-red-700');
+    expect(statusDiv).toHaveClass('badge-error');
   });
 
-  it('renders empty editor if no data or error', () => {
-    render(<ResponseViewer response={{ status: 204 }} />);
-    expect(screen.getByText(/Response Status: 204/)).toBeDefined();
+  it('renders empty editor if no data or error, and yellow status', () => {
+    renderWithTheme(<ResponseViewer response={{ status: 0 }} />);
+    expect(screen.getByText(/Response Status: 0/)).toBeDefined();
+    const statusDiv = screen.getByText(/Response Status:/);
+    expect(statusDiv).toHaveClass('badge-warning');
   });
 });
