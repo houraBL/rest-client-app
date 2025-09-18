7805ea2effb4cf086ffee1f6342e38131a717186
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-09-19 04:31:10 +0800

    test: update test

--- src/components/AuthPage/authPage.test.tsx
+++ src/components/AuthPage/authPage.test.tsx
@@ -3,20 +3,39 @@ import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import AuthPage from './AuthPage';
-import { NextIntlClientProvider } from 'next-intl';
 import {
   logInAndSetCookie,
   registerAndSetCookie,
 } from '@/lib/firebase/authActions';
-import * as reactFirebaseHooks from 'react-firebase-hooks/auth';
-import { User } from 'firebase/auth';
-
-vi.mock('@/lib/firebase/authActions', () => ({
-  logInAndSetCookie: vi.fn(),
-  registerAndSetCookie: vi.fn(),
-}));
 
 const pushMock = vi.fn();
+
+const { setUserMock } = vi.hoisted(() => {
+  return {
+    setUserMock: vi.fn(),
+  };
+});
+
+vi.mock('@/lib/firebase/authActions', () => ({
+  logInAndSetCookie: vi.fn().mockResolvedValue({
+    uid: '123',
+    email: 'katy@test.com',
+    displayName: 'Katy',
+  }),
+  registerAndSetCookie: vi.fn().mockResolvedValue({
+    uid: '123',
+    email: 'katy@test.com',
+    displayName: 'Katy',
+  }),
+}));
+
+vi.mock('next-intl', async (importActual) => {
+  const actual = await importActual<typeof import('next-intl')>();
+  return {
+    ...actual,
+    useTranslations: () => (key: string) => key,
+  };
+});
 
 vi.mock('@/i18n/navigation', () => ({
   Link: ({ href, children }: { href: string; children: React.ReactNode }) => (
@@ -34,25 +53,27 @@ vi.mock('@/i18n/navigation', () => ({
   }),
 }));
 
-vi.mock('@/hooks/useAuth/useAuth', () => ({
-  useAuth: vi.fn().mockReturnValue({ user: null, loading: false }),
-}));
+vi.mock('@/hooks/useAuth/useAuth', () => {
+  return {
+    useAuth: vi.fn().mockReturnValue({
+      user: true,
+      setUser: setUserMock,
+      loading: false,
+    }),
+  };
+});
 
 describe('AuthPage', () => {
   beforeEach(() => {
     vi.clearAllMocks();
   });
-  
-  it('Login success calls logInAndSetCookie', async () => {
-    render(
-      <NextIntlClientProvider locale="en">
-        <AuthPage />
-      </NextIntlClientProvider>
-    );
 
-    const emailInput = screen.getByPlaceholderText(/enter your email/i);
-    const passwordInput = screen.getByPlaceholderText(/enter your password/i);
-    const logInBtn = screen.getByRole('button', { name: /log in/i });
+  it('Login success calls logInAndSetCookie', async () => {
+    render(<AuthPage />);
+
+    const emailInput = screen.getByPlaceholderText(/emailPlaceholder/i);
+    const passwordInput = screen.getByPlaceholderText(/passwordPlaceholder/i);
+    const logInBtn = screen.getByRole('button', { name: /signIn/i });
 
     await userEvent.type(emailInput, 'katy@test.com');
     await userEvent.type(passwordInput, '11111111k.');
@@ -64,16 +85,17 @@ describe('AuthPage', () => {
         '11111111k.'
       );
     });
+    expect(setUserMock).toBeCalledWith({
+      uid: '123',
+      email: 'katy@test.com',
+      displayName: 'Katy',
+    });
   });
 
   it('Login shows validation errors', async () => {
-    render(
-      <NextIntlClientProvider locale="en">
-        <AuthPage />
-      </NextIntlClientProvider>
-    );
+    render(<AuthPage />);
 
-    const logInBtn = screen.getByRole('button', { name: /log in/i });
+    const logInBtn = screen.getByRole('button', { name: /signIn/i });
     await userEvent.click(logInBtn);
     const emailError = await screen.findByText((content) =>
       content.toLowerCase().includes('valid email')
@@ -86,13 +108,9 @@ describe('AuthPage', () => {
     expect(passwordError).toBeInTheDocument();
   });
   it('Shows validation errors on register submit', async () => {
-    render(
-      <NextIntlClientProvider locale="en">
-        <AuthPage isInitialLogin={false} />
-      </NextIntlClientProvider>
-    );
+    render(<AuthPage isInitialLogin={false} />);
 
-    const signUpBtn = screen.getByRole('button', { name: /sign up/i });
+    const signUpBtn = screen.getByRole('button', { name: /signUp/i });
     await userEvent.click(signUpBtn);
     const emailError = await screen.findByText((content) =>
       content.toLowerCase().includes('valid email')
@@ -105,15 +123,11 @@ describe('AuthPage', () => {
     expect(passwordError).toBeInTheDocument();
   });
   it('Register success calls registerAndSetCookie', async () => {
-    render(
-      <NextIntlClientProvider locale="en">
-        <AuthPage isInitialLogin={false} />
-      </NextIntlClientProvider>
-    );
-    const nameInput = screen.getByPlaceholderText(/enter your name/i);
-    const emailInput = screen.getByPlaceholderText(/enter your email/i);
-    const passwordInput = screen.getByPlaceholderText(/enter your password/i);
-    const signUpBtn = screen.getByRole('button', { name: /sign up/i });
+    render(<AuthPage isInitialLogin={false} />);
+    const nameInput = screen.getByPlaceholderText(/namePlaceholder/i);
+    const emailInput = screen.getByPlaceholderText(/emailPlaceholder/i);
+    const passwordInput = screen.getByPlaceholderText(/passwordPlaceholder/i);
+    const signUpBtn = screen.getByRole('button', { name: /signUp/i });
 
     await userEvent.type(nameInput, 'Katy');
     await userEvent.type(emailInput, 'katy@test.com');
@@ -126,6 +140,11 @@ describe('AuthPage', () => {
         'katy@test.com',
         '11111111k.'
       );
+      expect(setUserMock).toBeCalledWith({
+        uid: '123',
+        email: 'katy@test.com',
+        displayName: 'Katy',
+      });
     });
   });
 });
