f11efc9a10792fff3727fabeb9e26b97a58a7530
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-09-20 03:20:04 +0800

    fix: remove replaceUrl from onChange handler

--- src/components/Client/MethodSelector/MethodSelector.tsx
+++ src/components/Client/MethodSelector/MethodSelector.tsx
@@ -4,23 +4,18 @@ import { Methods } from '@/types/methods';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/store/store';
 import { setMethod } from '@/store/clientSlice';
-import { replaceUrl } from '@/utils/replaceUrl';
-import { useRouter } from '@/i18n/navigation';
 
 export function MethodSelector() {
-  const router = useRouter();
   const dispatch = useDispatch();
   const method = useSelector((state: RootState) => state.client.method);
-  const clientState = useSelector((state: RootState) => state.client);
   const methods = Object.values(Methods);
 
   const handleChange = (value: Methods) => {
     dispatch(setMethod(value));
-    replaceUrl(router, { ...clientState, method: value });
   };
 
   return (
-    <div className="flex-1">
+    <div className="flex-1 min-w-fit">
       <select
         value={method}
         onChange={(e) => handleChange(e.target.value as Methods)}
