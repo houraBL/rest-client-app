4c608554ed42038af434ef5b51b466c003f74594
Anastasiia Blagoveshchenskaia <Anastasiia Blagoveshchenskaia>
2025-09-20 06:00:24 +0800

    fix: update auth logic to use session cookie; add refetch of api/me

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -5,21 +5,26 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   try {
     const token = (await cookies()).get('auth_token')?.value;
-
     if (!token) {
       return NextResponse.json({ user: null }, { status: 401 });
     }
 
-    const decoded = await adminAuth.verifyIdToken(token);
-
+    const decoded = await adminAuth.verifySessionCookie(token);
     return NextResponse.json({
       user: {
         uid: decoded.uid,
         email: decoded.email,
-        name: decoded.name ?? null,
+        displayName: decoded.name ?? null,
       },
     });
   } catch (err) {
+    (await cookies()).set('auth_token', '', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      path: '/',
+      maxAge: 0,
+    });
     return NextResponse.json({ user: null }, { status: 401 });
   }
 }
--- src/lib/firebase/authActions.ts
+++ src/lib/firebase/authActions.ts
@@ -7,15 +7,23 @@ import {
   logout,
 } from './firebase';
 import type { User } from 'firebase/auth';
+import { adminAuth } from '@/lib/firebase/firebaseAdmin';
+
+const SESSION_EXPIRES = 60 * 60 * 24 * 7 * 1000;
 
 export async function setAuthCookie(user: User) {
-  const token = await user.getIdToken();
-  (await cookies()).set('auth_token', token, {
+  const idToken = await user.getIdToken();
+
+  const sessionCookie = await adminAuth.createSessionCookie(idToken, {
+    expiresIn: SESSION_EXPIRES,
+  });
+
+  (await cookies()).set('auth_token', sessionCookie, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'strict',
     path: '/',
-    maxAge: 60 * 60 * 24 * 7,
+    maxAge: SESSION_EXPIRES / 1000,
   });
 }
 
