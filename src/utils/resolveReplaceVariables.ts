6cc3c2bd925b44c84724c6b7ffa3447382ec9acc
AleksandrKlesh <alex73kl@gmail.com>
2025-09-22 03:47:38 +0700

    fix: combine 2 functions

--- src/utils/resolveReplaceVariables.ts
+++ src/utils/resolveReplaceVariables.ts
@@ -1,33 +1,37 @@
 import { ClientState } from '@/store/clientSlice';
-import { replaceURLVariables } from './replaceURLVariables';
 import { getFinalUrl } from './getFinalUrl';
 
 export function resolveReplaceVariables(clientState: ClientState) {
   const { url, headers, body, variables, bodyHeader, method } = clientState;
 
-  const finalUrlValue = replaceURLVariables({
-    stringToReplace: url,
-    variables,
-  });
+  const replaceVariables = ({
+    stringToReplace,
+  }: {
+    stringToReplace: string;
+  }): string => {
+    if (!stringToReplace) return stringToReplace;
+    const reg = /{{\s*([^}\s]+)\s*}}/g;
+
+    return stringToReplace.replace(reg, (matchStr, text) => {
+      if (variables[text] !== undefined) {
+        return variables[text] as string;
+      }
+      return matchStr;
+    });
+  };
+
+  const finalUrlValue = replaceVariables({ stringToReplace: url });
 
   const finalHeaders: Record<string, string> = {};
+
   Object.entries(headers).forEach(([key, value]) => {
-    const finalKey = replaceURLVariables({
-      stringToReplace: key,
-      variables,
-    });
-
-    const finalValue = replaceURLVariables({
-      stringToReplace: value,
-      variables,
-    });
-
+    const finalKey = replaceVariables({ stringToReplace: key });
+    const finalValue = replaceVariables({ stringToReplace: value });
     finalHeaders[finalKey] = finalValue;
   });
 
-  const finalBody = replaceURLVariables({
+  const finalBody = replaceVariables({
     stringToReplace: body,
-    variables,
   });
 
   if (finalBody) {
