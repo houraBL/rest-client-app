e470f71023177b5710acb9292cd419f5828cc029
AleksandrKlesh <alex73kl@gmail.com>
2025-09-21 14:15:43 +0700

    fix: add check to decode and remove parseUrl

--- src/utils/decodeBase64.ts
+++ src/utils/decodeBase64.ts
@@ -1,4 +1,9 @@
 export function decodeBase64<T = unknown>(encoded: string): T {
-  const json = atob(encoded);
-  return JSON.parse(json) as T;
+  if (!encoded) return '' as T;
+  try {
+    const json = atob(encoded);
+    return JSON.parse(json) as T;
+  } catch {
+    return '' as T;
+  }
 }
